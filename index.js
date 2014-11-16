1954b78a31490e7538b267d779aee14c9fc20aff
Lou Huang <lou@louhuang.com>
2014-11-16 10:42:57 -0500

    Add data to base

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 'use strict';
 
 var PORT       = 3000,
+    MONGO_URI  = process.env.MONGOLAB_URI || 'mongodb://localhost/mta_status',
     http       = require('http'),
     path       = require('path'),
     express    = require('express'),
     bodyParser = require('body-parser'),
     winston    = require('winston'),
-    lib        = require('./lib')
+    mongo      = require('mongodb'),
+    monk       = require('monk'),
+    db         = monk('localhost:27017/nodetest1'),
+    lib        = require('./lib'),
 
 var app = express()
 
 // Express application setup
 app.set('port', process.env.PORT || PORT)
+app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(bodyParser.json())
+// Make our db accessible to our router
+app.use(function (req, res, next) {
+  req.db = db
+  next()
+})
 
 app.post('/post', function (req, res) {
-  console.log(req.body)
-  res.status(202).end()
+  var dump = req.body
+  console.log(dump)
+
+  var db         = req.db,
+      collection = db.get('dumps')
+
+  collection.insert(dump, function (err, doc) {
+    if (err) {
+      res.status(500).send('There was a problem adding the information to the database.')
+    } else {
+      res.status(200).end()
+    }
+  })
+
 })
 
 app.get('/', function (req, res) {
