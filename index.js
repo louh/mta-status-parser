45afbeef71ac7af020baf6cf346cde3622af0170
Lou Huang <lou@louhuang.com>
2014-11-16 01:18:43 -0500

    Remove XML parser into the mta-status-scraper repo. Set up post route

--- index.js
+++ index.js
@@ -4,46 +4,23 @@ var PORT    = 3000,
     http    = require('http'),
     path    = require('path'),
     express = require('express'),
-    winston = require('winston'),
-    xml     = require('xml2js').parseString
+    winston = require('winston')
 
 var app = express()
-
-var API = 'http://web.mta.info/status/serviceStatus.txt'
 
 // Express application setup
 app.set('port', process.env.PORT || PORT)
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.post('/post', function (req, res) {
+  console.log('req')
+  res.status(202).end()
+})
+
 app.get('/', function (req, res) {
 
-  var request = http.get(API, function (response) {
+  res.status(200).send('<h1>mta-status-parser</h1><p>hello world.</p>')
 
-    var stream = '',
-        data
-
-    response.on('data', function (chunk) {
-      stream += chunk
-    })
-
-    response.on('end', function () {
-
-      xml(stream, function (error, result) {
-        if (error) {
-          winston.error('Error!', error)
-          res.status(500).send(error)
-        } else {
-          winston.info('Retrieved!')
-          res.status(200).send(result)
-        }
-      })
-
-    })
-  })
-
-  request.on('error', function (err) {
-    console.log('Error: ' + err.message)
-  })
 })
 
 app.listen(app.get('port'), function () {
